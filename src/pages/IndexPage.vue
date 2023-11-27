162a9e4763be59fbe129ffbfefbe0765a705a000
Pedro <50713178+prgf87@users.noreply.github.com>
2023-11-27 20:19:30 +0000

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button @click="toggleMessage">Show/Hide</button>
+    <p v-if="showMessage">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "I don't even know you, Vue...",
+      showMessage: true,
+    };
+  },
+  methods: {
+    toggleMessage() {
+      this.showMessage = !this.showMessage;
+    },
+  },
+};
+</script>
+
+<style>
+p,
+button {
+  color: brown;
+  border: groove;
+  width: 33vw;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50px;
+  padding: 20px;
+}
+
+div {
+  display: grid;
+  justify-content: center;
+  align-items: center;
+}
+</style>
